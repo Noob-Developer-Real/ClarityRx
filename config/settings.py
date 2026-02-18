58a55e94079d67954daf7f8ba17eaeaf7cebba74
noob-dev <tejasvibajaj2@gmail.com>
2026-02-19 00:39:34 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
