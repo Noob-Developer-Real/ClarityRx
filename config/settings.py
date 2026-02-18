8494eff4239c4fa9494377850060c08de8012eed
noob-dev <tejasvibajaj2@gmail.com>
2026-02-18 23:55:11 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["clarityrx-mko4.onrender.com"]
 
 
 # Application definition
